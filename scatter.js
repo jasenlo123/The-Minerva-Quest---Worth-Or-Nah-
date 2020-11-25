24f5db639a3085809d9295356e631a01569e2407
Jasen Lo <jasenlo123@gmail.com>
2020-11-25 15:49:38 +0800

    Updates

---
+++ scatter.js
@@ -0,0 +1,376 @@
+const scatter_margin = { left:-300, right: 250, top: 30, bottom: 200 };
+
+scatter_width = window.innerWidth - scatter_margin.left - scatter_margin.right,
+scatter_height = window.innerHeight - scatter_margin.bottom
+
+const scatter =  d3.select("#scrolly2").select("figure")
+    .append("svg")
+    .attr("width", scatter_width + scatter_margin.left + scatter_margin.right)
+    .attr("height", scatter_height + scatter_margin.top + scatter_margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + scatter_margin.left + "," + scatter_margin.top + ")");;
+
+const scatter_innerWidth = scatter_width - scatter_margin.left - scatter_margin.right*1.1;
+const scatter_innerHeight = scatter_height - scatter_margin.bottom;
+const spread = scatter_innerHeight*0.98;
+const heightAdjust = -scatter_innerHeight/1.4;
+
+const xValue = d => d.price;
+const xLabel = "listing monthly rent (usd)";
+const yValue = d => d.city;
+const circleSize= d => d.review_number;
+const circleOppacity = d => d.rating;
+
+const scatter_cities = ["San Francisco",
+                "Seoul", 
+                "Hyderabad", 
+                "Berlin", 
+                "Buenos Aires",
+                "London", 
+                "Taipei"]
+
+const colorScale = d3.scaleOrdinal()
+                      .domain(scatter_cities)
+                      .range(d3.schemeSet2);
+                      
+const g = scatter.append('g')
+    .attr('transform', `translate(${scatter_margin.right},${scatter_margin.top})`);
+const xAxisG = g.append('g')
+    .attr('transform', `translate(0, ${scatter_innerHeight})`);
+const xAxisG2 = g.append('g')
+    .attr('transform', `translate(0, ${scatter_innerHeight})`);
+
+
+g.append('text')
+  .attr('class', 'axis-label')
+  .attr('x', (scatter_innerWidth / 2.5))
+  .attr('y', scatter_innerHeight +50)
+  .text(xLabel);
+
+
+const xScale = d3.scaleLinear();
+
+const xAxis = d3.axisBottom()
+  .scale(xScale)
+  .ticks(8)
+  .tickSize(-scatter_innerHeight);
+
+
+d3.csv('full_data.csv', data => {
+
+  //initiate x value scale
+  xScale
+    .domain([0, 800])
+    .range([0, scatter_innerWidth])
+
+  //initiate axis
+  xAxisG.call(xAxis);
+
+  const scatter_tooltip = d3
+  .select("main")
+      .append("div")
+      .attr("class", "scatter-svg-tooltip")
+      .style("position", "absolute")
+      .style("visibility", "hidden");
+  
+  const image_preview = d3
+      .select("main")
+          .append("div")
+          .attr("class", "scatter-image-preview")
+          .style("position", "absolute")
+          .style("visibility", "hidden");
+
+  g.selectAll('circle')
+         .data(data)
+          .enter()
+          .append('a')
+          .attr('xlink:href', function(d){return d.link})
+          .append('circle')
+          .attr('cx', d => xScale(xValue(d)))
+          .attr('cy', d => scatter_height/2+Math.random()*spread + heightAdjust )
+
+          //different colour for each city
+          .attr('fill', function(d) { return colorScale(d.city)})
+
+          //border
+          .attr("stroke", "black") 
+          .attr("stroke-opacity", 0.5)
+          .attr("stroke-width", 1) 
+          .style("visibility", "visible")
+  
+  g.append("line")
+          .attr("x1", xScale(750)+30)
+          .attr("y1", -5)
+          .attr("x2", xScale(750)+30)
+          .attr("y2", scatter_innerHeight)
+          .attr("stroke-width", 5)
+          .attr("stroke", "orange")
+          .attr("class", "moving_line")
+          .attr("visibility", "hidden");
+
+    g.append("text")
+          .attr("x", xScale(750)-20)
+          .attr("y", -40)
+          .text(750)
+          .attr("class", "moving_line_label")
+          .attr("visibility", "hidden");
+
+
+
+    function initiateLegend0() {
+    g.selectAll("legend_labels")
+      .data(scatter_cities)
+      .enter()
+      .append("text")
+      .text(function(d){ return d})
+      .attr("x", function(d,i){ return scatter_innerWidth/10+40 + i*(scatter_innerWidth/50+scatter_innerWidth/10)})
+      .attr("y", scatter_innerHeight*1.2+15) 
+
+    g.selectAll("legend_rects")
+          .data(scatter_cities)
+          .enter()
+          .append("rect")
+          .attr("id", "legend_rect")
+          .attr('fill', function(d) { return colorScale(d)})
+          .attr("width", scatter_innerWidth/50)
+          .attr("height", scatter_innerHeight/20)
+          .attr("x", function(d,i){ return scatter_innerWidth/10 + i*(scatter_innerWidth/50+scatter_innerWidth/10)})
+          .attr("y", scatter_innerHeight*1.2) 
+          .on("mouseover", function(d) {
+            var legendUnderMouse = this;
+              g.selectAll("#legend_rect")
+                  .attr('opacity',function () {
+                  return (this === legendUnderMouse) ? 1.0 : 0.2;
+                    })
+              g.selectAll("circle")
+                  .attr('opacity',function (e) {
+                    return (e.city == d) ? 1.0 : 0.2;
+                      })
+                  .style('stroke-width',function (e) {
+                        return (e.city == d) ? "2" : "1";
+                          })
+
+
+            })
+          .on("mouseout", function() {
+              // change the selection style
+            g.selectAll("circle")
+              .style('stroke-width', '1')
+              .attr('opacity', 1)
+              scatter_tooltip.style("visibility", "hidden");
+            d3.selectAll("#legend_rect")
+              .attr('opacity', 1);
+
+            })
+        
+    }
+    initiateLegend0()
+
+
+  // using d3 for convenience
+  var scrolly2 = d3.select("#scrolly2");
+  var article2 = scrolly2.select("article");
+  var step2 = article2.selectAll(".step2");
+  var figure2 = scrolly2.select("figure");
+
+
+  // initialize the scrollama
+  var scroller2 = scrollama();
+
+  function calc (finaid) {
+    return (3000 - finaid)/4
+  }
+
+  function handleStepProgress2(response) {
+    handleMouseEvent0(response);
+    handleMouseEvent1(response)
+
+      function handleMouseEvent0 (response) {
+        if (response.index == 0){
+
+
+            g.selectAll('circle')
+              .transition()
+              .duration(500)
+              .attr('fill-opacity', 1)
+              .attr('r', 5)
+
+
+            figure2.selectAll("circle")
+                .style('visibility', function(d) {            
+                if (d.price <= calc((1-response.progress)*3000)) {return "visible"}
+                else { return "hidden" };
+                })
+                .on("mouseover", function(d){
+                    var url = "<img src= "+ d.image + ">";
+                    image_preview 
+                        .style("visibility", "visible")
+                        .html(url)
+
+                    d3.select(this)
+                        .style("stroke-opacity", 1)
+                        .style('stroke-width', 2);
+
+                    g.append("text")
+                        .attr("x", xScale(d.price)-20)
+                        .attr("y", -20)
+                        .text(`$${Math.round(d.price)}`)
+                        .attr("class", "mouseover");
+
+                    g.append("line")
+                        .attr("x1", xScale(d.price)-5)
+                        .attr("x2", xScale(calc((1-response.progress)*3000)))
+                        .attr("stroke-width", 1)
+                        .attr("stroke", "orange")
+                        .attr("class", "mouseover");
+                    g.append("line")
+                        .attr("x1", xScale(d.price)-5)
+                        .attr("x2", xScale(d.price)-5)
+                        .attr("y1", -5)
+                        .attr("y2", scatter_innerHeight)
+                        .attr("stroke-width", 1)
+                        .attr("stroke", "orange")
+                        .attr("class", "mouseover");
+
+                    figure2.select("#figure_caption")
+                        .style("display", "none");
+
+                    figure2.select("#mouseover_figure_caption")
+                        .style("display", "block")
+                        .html(`
+                        (Residential Fee - Independent Fee - Scholarship) / 4 months ) - Listing Rent Per Month  = Money Saved! <br> 
+                        ( $ 5500 - $ 2500 - $ ${Math.round((1-response.progress)*3000)} ) / 4 months ) -  $ ${Math.round(d.price)} = Money Saved!<br> <br> 
+                        Opting out of Minerva housing for this 
+                        <span>${d.roomtype}</span> in
+                        <span>${d.detailed_location}</span>,
+                        <span>${d.city}</span> with
+                        <span>${d.beds -1 } roomies </span> 
+                        would save you <br> <span>$
+                        ${Math.round(calc((1-response.progress)*3000))} -  $ ${Math.round(d.price)} = $ ${Math.round(calc((1-response.progress)*3000) - d.price)}
+                        per month </span> if you get $ <span> ${Math.round((1-response.progress)*3000)} </span>in fin aid each semester. 
+                        `)
+                   scatter_tooltip
+                        .style("visibility", "visible")
+                        .text(`${d.full_rating}`)
+                
+
+                })
+                .on("mousemove", function() {
+                  image_preview  
+                    .style("top", d3.event.pageY + 50+ "px")
+                    .style("left", d3.event.pageX  - scatter_innerWidth/12 + "px")
+                  
+                  scatter_tooltip
+                  .style("top", d3.event.pageY + 10 + "px")
+                  .style("left", d3.event.pageX  - scatter_innerWidth/12 + "px")
+                })
+                .on("mouseout", function() {
+                          // change the selection style
+                      d3.select(this)
+                        .style('stroke-width', '1')
+                        .style("stroke-opacity", 0.5);
+                      d3.selectAll(".mouseover")
+                        .style("visibility", "hidden");
+
+                      figure2.select("#figure_caption")
+                        .style("display", "block");
+
+                      figure2.select("#mouseover_figure_caption")
+                        .style("display", "none");  
+
+                      image_preview.style("visibility", "hidden");
+                      scatter_tooltip.style("visibility", "hidden");
+
+                })
+
+            figure2.select("#figure_caption")
+                .html(`This viz shows the independent housing options found on Airbnb that make financial sense to those who receive under $6,000 per year or $3,000 per semester in scholarships. Scroll to vary the scholarship amount... <br><br> If you receive $ <span>${Math.round((1-response.progress)*3000)}</span> in in scholarship per semester, you would save money by living independently in listings (left of the <span style="color: orange;"> orange line</span>) that cost below $ <span>${Math.round(calc((1-response.progress)*3000))}</span>. Mouse over any listing, represented by the circles, to find out why and how much!`)
+                
+      
+            figure2.select(".moving_line")
+              .attr("visibility", "visible")
+              .attr("x1", xScale(calc((1-response.progress)*3000)))
+              .attr("x2", xScale(calc((1-response.progress)*3000)))
+            figure2.select(".moving_line_label")
+              .attr("visibility", "visible")
+              .attr("x", xScale(calc((1-response.progress)*3000))-20)
+              .text(`$${Math.round(calc((1-response.progress)*3000))}`)
+       }
+      }
+      
+      function handleMouseEvent1 (response) {
+        if (response.index == 1){
+
+            g.selectAll('circle')
+                .attr('xlink:href', function(d){return d.link})
+
+            figure2.select("#figure_caption")
+                .html(`<br>
+                For anyone serious about opting out of Minerva housing and curious about what Airbnb options look like, click on each bubble to get to the listing page. The Quest also scraped the ratings from these listings. Use the legend below to note which cities have more listings, and which of these are not too sus given their ratings. <br>
+                <span> Larger Bubbles = More Reviews | Deeper Colour = Better Ratings</span>`)
+
+            figure2.selectAll('circle')
+                .attr('xlink:href', function(d){return d.link})
+                .transition()
+                .duration(500)
+                //opacity depends on ratings
+                .attr('fill-opacity', d => (circleOppacity(d)-3) / 2.5)
+                //size depends on the number of reviews
+                .attr('r', d => circleSize(d)/ 20 + 3.5)
+            
+            function intiateLegend1() {
+            g.selectAll("legend_rects")
+                .on("mouseover", function(d) {
+                  var legendUnderMouse = this;
+                    g.selectAll("#legend_rect")
+                        .attr('opacity',function (d) {
+                        return (this === legendUnderMouse) ? 1.0 : 0.2;
+                          })
+            g.selectAll("circle")
+                        .attr('opacity',function (e) {
+                          return (e.city == d) ? (circleOppacity(d)-3) / 2.5 : 0.2;
+                            })
+
+              })
+            }
+            intiateLegend1()
+        }
+      }
+      
+
+  }
+
+
+  function resize2() {
+    var min = window.innerHeight*2;
+    step2.style("height", min + "px");
+    scroller2.resize();
+
+    var figureHeight = window.innerHeight;
+    var figureMarginTop = (window.innerHeight - figureHeight);
+
+    figure2
+      .style("height", figureHeight + "px")
+      .style("top", figureMarginTop + "px");
+  }
+
+  function init() {
+    scroller2
+      .setup({
+        step: "#scrolly2 article .step2",
+        progress: true,
+        offset: 0.5,
+      })
+      .onStepProgress(handleStepProgress2);
+
+    // setup resize event
+    resize2();
+    window.addEventListener("resize", resize2);
+  }
+
+  // kick things off
+  init();  
+
+});
+
---
+++ scroller.js
@@ -0,0 +1,452 @@
+
+// set the dimensions and margins of the graph
+var margin = {top: 100, right: 100, bottom: 300, left: 50},
+width = window.innerWidth*0.7 - margin.left - margin.right,
+height = window.innerHeight - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var hist = d3.select("#scrolly").select("figure")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Global Variables! 
+const ticks = 15
+const minervaCost = 750
+const minervaCostPlus = 1375
+const histHeight = 100
+const histColour = "#fed8b1"
+const cities = ["San Francisco",
+                "San Francisco",
+                "Seoul", 
+                "Hyderabad", 
+                "Berlin", 
+                "Buenos Aires",
+                "London", 
+                "Taipei"]
+
+
+// get the data
+d3.csv("prices_data.csv", function(data) {
+
+  const tooltip = d3
+  .select("main")
+      .append("div")
+      .attr("class", "svg-tooltip")
+      .style("position", "absolute")
+      .style("visibility", "hidden");
+
+  //axis labels
+  function initiateAxisLabels () {
+      //y-axis
+      hist.append("text")
+          .attr("class","axis-label")
+          .text("number of")
+          .attr("x", -30)
+          .attr("y", -30)
+      hist.append("text")
+          .attr("class","axis-label")
+          .text("listings")
+          .attr("x", -30)
+          .attr("y", -15)
+
+      //x-axis
+      hist.append("text")
+          .attr("class","axis-label")
+          .text("listing monthly")
+          .attr("x", width-100)
+          .attr("y", height+40)
+      hist.append("text")
+          .attr("class","axis-label")
+          .text("rent (usd)")
+          .attr("x", width-100)
+          .attr("y", height+55)
+
+  }
+  initiateAxisLabels()
+
+
+    // X axis: scale:
+    var max = d3.max(data, function(d) { return +d.price })
+    var x = d3.scaleLinear()
+      .domain([0, max]) 
+      .range([0, width]);
+    // X axis: draw:
+    hist.append("g")
+    .attr("id", "x-axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+  
+    // Y axis: scale:
+    var y = d3.scaleLinear()
+      .range([height, 0]);
+    y.domain([0, histHeight ]);   
+    // Y axis: draw:
+    hist.append("g")
+      .call(d3.axisLeft(y)); 
+
+  // Get a subset of the data based on the group
+  function getFilteredData(data, group) {
+    return data.filter(function(point) { return point.city === group; });
+  }
+
+  // Helper function to add new points to our data
+  function enterPoints(data) {
+
+
+    var max = d3.max(data, function(d) { return +d.price })+ 200
+    var x = d3.scaleLinear()
+      .domain([0, max]) 
+      .range([0, width]);
+    
+    // set the parameters for the histogram
+    var histogram = d3.histogram()
+    .value(function(d) { return d.price; })   // I need to give the vector of value
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(ticks)); // then the numbers of bins
+
+    var bins = histogram(data);
+
+
+    // append the bar rectangles to the svg element
+    hist.selectAll("rect")
+        .data(bins)
+        .enter()
+        .append("rect")
+          .attr("x", 1)
+          .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+          .transition()
+          .duration(500)
+          .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+          .attr("height", function(d) { return height - y(d.length); })
+          .style("fill", "white")
+          .style("stroke", "darkOrange" )
+          .style('stroke-width', '1')
+
+
+
+    // select all rect to initiate tooltip!
+    function initiateToolTip () {
+        d3.select('figure').selectAll("rect")
+        .on("mouseover", function(d) {
+          // change the selection style
+          d3.select(this)
+           .transition()
+           .duration(200)
+            .style("fill", "white")
+            .style("stroke", histColour)
+            .style('stroke-width', '2');
+          // make the tooltip visible and update its text
+          tooltip
+            .style("visibility", "visible")
+            .text(`There were ${d.length} Airbnb listings \n between $${d.x0} and $${d.x1}.`);
+        })
+        .on("mousemove", function() {
+          tooltip
+            .style("top", d3.event.pageY - 50 + "px")
+            .style("left", d3.event.pageX + 10 + "px");
+        })
+        .on("mouseout", function() {
+          // change the selection style
+          d3.select(this)
+            .transition()
+            .duration(200)
+            .style('stroke-width', '1')
+            .style("stroke", "white")
+            .style("fill", histColour);
+    
+          tooltip.style("visibility", "hidden");
+        });
+    }
+    initiateToolTip ();
+
+
+ 
+  }
+
+  function exitPoints(data) {
+
+    var max = d3.max(data, function(d) { return +d.price })+ 200
+    var x = d3.scaleLinear()
+      .domain([0, max]) 
+      .range([0, width]);
+
+    // set the parameters for the histogram
+    var histogram = d3.histogram()
+    .value(function(d) { return d.price; })   // I need to give the vector of value
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(ticks)); // then the numbers of bins
+
+    var bins = histogram(data);
+
+    hist.selectAll("rect")
+        .data(bins)
+        .exit()
+        .remove();
+
+
+  }
+  function updatePoints(data) {
+    // remove previous line
+      hist.selectAll("line").remove();
+      hist.selectAll(".scroller_anno").remove();
+
+
+    var max = d3.max(data, function(d) { return +d.price })+ 200
+    var x = d3.scaleLinear()
+      .domain([0, max]) 
+      .range([0, width]);
+
+    d3.selectAll("#x-axis")
+        .call(d3.axisBottom(x));
+
+    // set the parameters for the histogram
+    var histogram = d3.histogram()
+    .value(function(d) { return d.price; })   // I need to give the vector of value
+    .domain(x.domain())  // then the domain of the graphic
+    .thresholds(x.ticks(ticks)); // then the numbers of bins
+
+    var bins = histogram(data);
+
+
+    hist.selectAll("rect")
+        .data(bins)
+        .transition()
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height", function(d) { return height - y(d.length); })
+        .style("fill", histColour )
+        .style("stroke", "white")
+        .style('stroke-width', '1')
+
+
+      d3.selectAll(".minerva-chart-label")
+          .style("visibility", "hidden");
+      d3.selectAll(".median-chart-label")
+          .style("visibility", "hidden");
+
+    
+ 
+ 
+    function drawLineMedian (value, colour) {
+        hist.append("text")
+            .attr("class","median-chart-label")
+            .text("median")
+            .attr("x", x(value)-20)
+            .attr("y", height+50)
+            .style('fill', 'black')
+        hist.append("text")
+            .attr("class","median-chart-label")
+            .text("airbnb")
+            .attr("x",x(value)-20)
+            .attr("y", height+80)
+            .style('fill', 'black')
+        hist.append("text")
+            .attr("class","median-chart-label")
+            .text("listing")
+            .attr("x",x(value)-20)
+            .attr("y", height+110)
+            .style('fill', 'black')
+
+        hist.append("line")
+          .attr("x1", x(value) )
+          .attr("x2", x(value) )
+          .attr("y1", y(0))
+          .attr("y2", y(0))
+          .attr("stroke", colour)
+          .attr("stroke-width", "3") 
+           .transition()
+           .duration(300)
+           .attr("x1", x(value) )
+           .attr("x2", x(value) )
+           .attr("y1", y(0))
+           .attr("y2", y(histHeight))
+        
+        hist.append("text")
+           .attr("class","line-label")
+           .text(`$${value}`)
+           .attr("x", x(value)-20)
+           .attr("y", -15)
+           .style('fill', 'black')
+           
+      }
+    function drawLineMinerva (value, colour) {
+      hist.append("line")
+            .attr("stroke", colour)
+            .attr("stroke-width", "3") 
+            .attr("x1", x(value) )
+            .attr("x2", x(value) )
+            .attr("y1", y(0))
+            .attr("y2", y(0))
+            .transition()
+            .duration(300)
+            .attr("x1", x(value) )
+            .attr("x2", x(value) )
+            .attr("y1", y(0))
+            .attr("y2", y(histHeight))
+            ;
+         hist.append("text")
+             .attr("class","minerva-chart-label")
+             .text("minerva ")
+             .attr("x", x(value)-20)
+             .attr("y", height+65)
+             .style('fill', 'darkOrange')
+         hist.append("text")
+             .attr("class","minerva-chart-label")
+             .text("housing")
+             .attr("x", x(value)-20)
+             .attr("y", height+95)
+             .style('fill', 'darkOrange')
+
+          hist.append("text")
+             .attr("class","line-label")
+             .text(`$${value}`)
+             .attr("x", x(value)-20)
+             .attr("y", -15)
+             .style('fill', 'darkOrange')
+ 
+    }
+       d3.selectAll(".line-label")
+           .style("visibility", "hidden")
+       drawLineMinerva(minervaCost, "darkOrange") 
+       //median line
+       var median = d3.median(data, d => d.price)
+       drawLineMedian(median, "black") 
+ 
+
+    function calculateProportionCheaper(data) {
+        var entries = data.length
+
+        var cheaper = data.filter(function(cost) { return cost.price < 750; }).length
+
+        hist.append("text")
+        .text(`${cheaper} / ${entries} (~${Math.round((cheaper/entries)*100)}%)`)
+        .attr("class", "scroller_anno")
+        .attr("font-weight", 1000)
+        .attr("x", width-150)
+        .attr("y", height -250)
+        .attr("fill", "darkOrange")
+
+
+        hist.append("text")
+        .text(`listings scraped`)
+        .attr("class", "scroller_anno")
+        .attr("x", width-150)
+        .attr("y", height -230)
+
+
+        hist.append("text")
+        .text(`were cheaper `)
+        .attr("class", "scroller_anno")
+        .attr("x", width-150)
+        .attr("y", height -210)
+
+
+        hist.append("text")
+        .text(`than Minerva`)
+        .attr("class", "scroller_anno")
+        .attr("x", width-150)
+        .attr("y", height -190)
+
+      }
+      calculateProportionCheaper(data)
+    }
+    
+  
+
+
+  // using d3 for convenience, and storing a selected elements
+  var main = d3.select("main");
+  var scrolly = main.select("#scrolly");
+  var figure = scrolly.select("figure");
+  var article = scrolly.select("article");
+  var step = article.selectAll(".step");
+
+  // initialize the scrollama
+  var scroller = scrollama();
+
+  // resize function to set dimensions on load and on page resize
+  function handleResize() {
+
+      // 1. update height of step elements
+      var stepH = Math.floor(window.innerHeight * 0.85);
+
+      step.style("height", stepH + "px");
+      
+
+      var figureHeight = window.innerHeight / 1.2;
+      var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+
+      figure
+        .style("height", figureHeight + "px")
+        .style("top", figureMarginTop + "px");
+
+      // 3. tell scrollama to update new element dimensions
+      scroller.resize();
+  }
+
+  // scrollama event handlers
+  function handleStepEnter(response) {
+
+
+      // response = { element, direction, index }
+      console.log(response)
+
+
+      // fade in current step
+      step.classed('is-active', function (d, i) {
+        return i === response.index;
+      })
+
+      // update graphic based on step here
+      var $groupSelector = cities[response.index]
+      var groupData = getFilteredData(data, $groupSelector);
+
+      if (response.index == 0 && response.direction == "down") {
+           enterPoints(groupData);
+      }
+      else {
+          updatePoints(groupData);
+          exitPoints(groupData);
+      }
+  }
+
+  function setupStickyfill() {
+      d3.selectAll(".sticky").each(function() {
+        Stickyfill.add(this);
+      });
+  }
+    
+
+  // kick-off code to run once on load
+  function init() {
+
+    setupStickyfill();
+
+      	// 1. call a resize on load to update width/height/position of elements
+	    handleResize();
+
+	    // 2. setup the scrollama instance
+    	// 3. bind scrollama event handlers (this can be chained like below)
+      scroller
+        .setup({
+          step: "#scrolly article .step", // the step elements
+          offset: 0.5, // set the trigger to be 1/2 way down screen
+          //debug: true, // display the trigger offset for testing
+        })
+        .onStepEnter(handleStepEnter)
+
+      // setup resize event
+      window.addEventListener('resize', handleResize);
+    
+  }
+
+  // start it up
+  init();
+
+
+
+});
